5db2e78af859aa4197bb0100624c4db29353da59
Naveen Kumar Peddi <peddin5739@uhcl.edu>
2023-10-19 18:00:54 -0500

    Customize the Colors of the shoe(used_ContestAPT)

--- project_configurator/src/App.js
+++ project_configurator/src/App.js
@@ -1,32 +1,43 @@
 import "./App.css";
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import Colors from "./sharingcolors.jsx";
 
 import ShoeModel from "./ShoeModel";
+import Customize from "./customize";
 
 function App() {
+  const [color, setColor] = useState("black");
+  const [mesh, setMesh] = useState("shoe_1");
+
   return (
-    <div className="container">
-      <div className="component left"></div>
+    <Colors.Provider value={{ color, setColor, mesh, setMesh }}>
+      <div className="container">
+        <div className="component left">
+          <Customize />
+        </div>
 
-      <div className="component right">
-        <Canvas>
-          {/* Camera Setup */}
-          <PerspectiveCamera makeDefault position={[0, 2, 5]} />
-          <OrbitControls />
+        <div className="component right">
+          {" "}
+          {/* Updated to .component.right */}
+          <Canvas>
+            {/* Camera Setup */}
+            <PerspectiveCamera makeDefault position={[0, 2, 5]} />
+            <OrbitControls />
 
-          {/* Lighting */}
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[2, 2, 2]} intensity={1} />
+            {/* Lighting */}
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[2, 2, 2]} intensity={1} />
 
-          {/* Shoe Model */}
-          <Suspense fallback={null}>
-            <ShoeModel />
-          </Suspense>
-        </Canvas>
+            {/* Shoe Model */}
+            <Suspense fallback={null}>
+              <ShoeModel />
+            </Suspense>
+          </Canvas>
+        </div>
       </div>
-    </div>
+    </Colors.Provider>
   );
 }
 
